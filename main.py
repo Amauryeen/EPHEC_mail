0bf21addb900f14259246e4af96239894b65303e
Amaury <iamaury1380@gmail.com>
2022-12-14 15:52:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 import smtplib
 import poplib
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Mail:
+
+    def __init__(self, receivers, sender=os.getenv('OUTLOOK_EMAIL'), subject='', body='', cc=None, cci=None):
+        if cc is None:
+            cc = []
+        if cci is None:
+            cci = []
+
+        # Amaury
+        # Check if receivers is valid
+        # TODO: receivers is a list, has at least 1 entry and maximum 100 entries, all entries are e-mails
+
+        # Check if subject is valid
+        # TODO: subject is less than 201 characters
+
+        # Check if body is valid
+        # TODO: body is less than 1001 characters
+
+        # Check if cc is valid
+        # TODO: cc is a list, has a maximum of 100 entries, all entries are e-mails
+
+        # Check if cci is valid
+        # TODO: cci is a list, has a maximum of 100 entries, all entries are e-mails
+
+        self.__sender = sender
+        self.__receivers = receivers
+        self.__subject = subject
+        self.__body = body
+        self.__cc = cc
+        self.__cci = cci
+
+        # TODO: create properties for private variables
+
+    def send_mail(self):
+        # Amaury
+        pass
+
 
 def main():
     pass
