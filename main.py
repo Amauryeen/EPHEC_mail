8ca069f1dd38f34aff169f9a6008b074b2b0e243
Amaury <iamaury1380@gmail.com>
2022-12-14 23:38:58 +0100

    Mail check method

--- main.py
+++ main.py
@@ -71,7 +71,8 @@ class Mail:
     # METHODS
 
     def check_mail(self, mail):
-        pass
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        return re.fullmatch(regex, mail)
 
     def send_mail(self):
         # Amaury
